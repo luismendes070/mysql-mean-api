8b0226671e2681daa517e7cad9ffa533cd089b73
Luis Mendes <luis.mendes070@outlook.com.br>
2020-10-27 21:46:43 -0300

    mysql mean api

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+//var mongodb = require("mongodb");
+//var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
+var CONTACTS_COLLECTION = "products";
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,23 +11,22 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'db4free.net',
+  user     : 'desafiofullstack',
+  password : '*5xt#AU4gtjVUsA',
+  database : 'desafiodb'
 });
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
 
 // CONTACTS API ROUTES BELOW
 /*  "/api/contacts"
