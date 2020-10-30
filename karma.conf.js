da6d2adf6674fc6656422f0a8f3363ef9d07c4a0
Luis Mendes <luis.mendes070@outlook.com.br>
2020-10-30 12:26:13 -0300

    make it better

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -16,7 +16,7 @@
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/products'),
+      dir: require('path').join(__dirname, './coverage/angular.io-example'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
@@ -29,4 +29,4 @@
     singleRun: false,
     restartOnFileChange: true
   });
-
+};
