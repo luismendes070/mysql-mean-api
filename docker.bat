2854620ff56c0e4cc89745cb009c35bd19d9ff10
Luis Mendes <luis.mendes070@outlook.com.br>
2020-11-03 02:32:06 -0300

    sem pug jade express

---
+++ docker.bat
@@ -0,0 +1,3 @@
+docker push luismendes070/ng-products-api:tagname
+//docker tag local-image:products new-repo:products
+//docker push new-repo:ng-products-api
--- src/server.js
+++ src/server.js
@@ -75,6 +75,40 @@ app.post("/api/products", function(req, res) {
   }
 });
 
+var express = require('express')
+  , routes  = require('./routes')
+  , user    = require('./routes/user')
+  , http    = require('http')
+  , path    = require('path')
+  , db      = require('./models');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/products', produt.list);
+
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+});
+
 var restify = require("restify");
 //curl -is http://localhost:4200/api/products -H 'accept: text/plain'
 
