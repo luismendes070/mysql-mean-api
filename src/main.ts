2af02b801aba26a1dc8ccde6cb80804d0b3053ab
Luis Mendes <luis.mendes070@outlook.com.br>
2020-11-11 21:49:19 -0300

    melhoria rxjs

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { environment } from './environments/environment';
 
 // import { Local } from '@angular/common';
 
-// import { defineCustomElements } from '@ionic/pwa-elements/loader';
+ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 //import { Firebase } from '@ionic-native/firebase/ngx';
 
