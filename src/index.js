7d3a1bceff856c607d738abf8b91b2308b21381c
Luis Mendes <machadoluismendes@gmail.com>
2020-11-16 17:29:16 -0300

    audit fix update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+ // import { Sequelize, Jsonproduct } from "./database.ts";
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
+
 function component() {
   const element = document.createElement('div');
 
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    filename: 'my-first-webpack.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  },
+  
+  module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
+
+  
+};
