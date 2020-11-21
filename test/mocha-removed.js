deec39dd69c81b2a008e8b0b266c3fa265704c33
Luis Mendes <machadoluismendes@gmail.com>
2020-11-21 17:09:55 -0300

    webpack increment jest removed mocha removed

---
+++ test/mocha-removed.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+describe("Array", function () {
+  describe("#indexOf()", function () {
+    it("should mocha load removed", function () {
+      //assert.equal([1, 2, 3].indexOf(4), -1);
+      fail(true);
+    });
+  });
+});
